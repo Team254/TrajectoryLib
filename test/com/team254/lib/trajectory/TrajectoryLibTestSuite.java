89011d774b8e6c660951e964c0d68eb485848065
Jared Russell <jared@team341.com>
2014-02-01 15:30:34 -0800

    Added Test suite.

---
+++ test/com/team254/lib/trajectory/TrajectoryLibTestSuite.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.team254.lib.trajectory;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Test suite.
+ * 
+ * @author Jared341
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({com.team254.lib.trajectory.TrajectoryGeneratorTests.class})
+public class TrajectoryLibTestSuite {
+
+  @BeforeClass
+  public static void setUpClass() throws Exception {
+  }
+
+  @AfterClass
+  public static void tearDownClass() throws Exception {
+  }
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+  
+}
