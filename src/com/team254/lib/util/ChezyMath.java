081431b6ff59af1b8e87d80bdc5471c597b8b92f
Jared Russell <jared@team341.com>
2014-02-01 13:36:07 -0800

    Initial commit

---
+++ src/com/team254/lib/util/ChezyMath.java
@@ -0,0 +1,176 @@
+package com.team254.lib.util;
+
+/**
+ * This class holds a bunch of static methods and variables needed for
+ * mathematics
+ */
+public class ChezyMath {
+  // constants
+
+  static final double sq2p1 = 2.414213562373095048802e0;
+  static final double sq2m1 = .414213562373095048802e0;
+  static final double p4 = .161536412982230228262e2;
+  static final double p3 = .26842548195503973794141e3;
+  static final double p2 = .11530293515404850115428136e4;
+  static final double p1 = .178040631643319697105464587e4;
+  static final double p0 = .89678597403663861959987488e3;
+  static final double q4 = .5895697050844462222791e2;
+  static final double q3 = .536265374031215315104235e3;
+  static final double q2 = .16667838148816337184521798e4;
+  static final double q1 = .207933497444540981287275926e4;
+  static final double q0 = .89678597403663861962481162e3;
+  static final double PIO2 = 1.5707963267948966135E0;
+  static final double nan = (0.0 / 0.0);
+  // reduce
+
+  private static double mxatan(double arg) {
+    double argsq, value;
+
+    argsq = arg * arg;
+    value = ((((p4 * argsq + p3) * argsq + p2) * argsq + p1) * argsq + p0);
+    value = value / (((((argsq + q4) * argsq + q3) * argsq + q2) * argsq + q1) * argsq + q0);
+    return value * arg;
+  }
+
+  // reduce
+  private static double msatan(double arg) {
+    if (arg < sq2m1) {
+      return mxatan(arg);
+    }
+    if (arg > sq2p1) {
+      return PIO2 - mxatan(1 / arg);
+    }
+    return PIO2 / 2 + mxatan((arg - 1) / (arg + 1));
+  }
+
+  // implementation of atan
+  public static double atan(double arg) {
+    if (arg > 0) {
+      return msatan(arg);
+    }
+    return -msatan(-arg);
+  }
+
+  // implementation of atan2
+  public static double atan2(double arg1, double arg2) {
+    if (arg1 + arg2 == arg1) {
+      if (arg1 >= 0) {
+        return PIO2;
+      }
+      return -PIO2;
+    }
+    arg1 = atan(arg1 / arg2);
+    if (arg2 < 0) {
+      if (arg1 <= 0) {
+        return arg1 + Math.PI;
+      }
+      return arg1 - Math.PI;
+    }
+    return arg1;
+
+  }
+
+  // implementation of asin
+  public static double asin(double arg) {
+    double temp;
+    int sign;
+
+    sign = 0;
+    if (arg < 0) {
+      arg = -arg;
+      sign++;
+    }
+    if (arg > 1) {
+      return nan;
+    }
+    temp = Math.sqrt(1 - arg * arg);
+    if (arg > 0.7) {
+      temp = PIO2 - atan(temp / arg);
+    } else {
+      temp = atan(arg / temp);
+    }
+    if (sign > 0) {
+      temp = -temp;
+    }
+    return temp;
+  }
+
+  // implementation of acos
+  public static double acos(double arg) {
+    if (arg > 1 || arg < -1) {
+      return nan;
+    }
+    return PIO2 - asin(arg);
+  }
+
+  /**
+   * Get the difference in angle between two angles.
+   *
+   * @param from The first angle
+   * @param to The second angle
+   * @return The change in angle from the first argument necessary to line up
+   * with the second. Always between -Pi and Pi
+   */
+  public static double getDifferenceInAngleRadians(double from, double to) {
+    return boundAngleNegPiToPiRadians(to - from);
+  }
+
+  /**
+   * Get the difference in angle between two angles.
+   *
+   * @param from The first angle
+   * @param to The second angle
+   * @return The change in angle from the first argument necessary to line up
+   * with the second. Always between -180 and 180
+   */
+  public static double getDifferenceInAngleDegrees(double from, double to) {
+    return boundAngleNeg180to180Degrees(to - from);
+  }
+
+  public static double boundAngle0to360Degrees(double angle) {
+    // Naive algorithm
+    while (angle >= 360.0) {
+      angle -= 360.0;
+    }
+    while (angle < 0.0) {
+      angle += 360.0;
+    }
+    return angle;
+  }
+
+  public static double boundAngleNeg180to180Degrees(double angle) {
+    // Naive algorithm
+    while (angle >= 180.0) {
+      angle -= 360.0;
+    }
+    while (angle < -180.0) {
+      angle += 360.0;
+    }
+    return angle;
+  }
+
+  public static double boundAngle0to2PiRadians(double angle) {
+    // Naive algorithm
+    while (angle >= 2.0 * Math.PI) {
+      angle -= 2.0 * Math.PI;
+    }
+    while (angle < 0.0) {
+      angle += 2.0 * Math.PI;
+    }
+    return angle;
+  }
+
+  public static double boundAngleNegPiToPiRadians(double angle) {
+    // Naive algorithm
+    while (angle >= Math.PI) {
+      angle -= 2.0 * Math.PI;
+    }
+    while (angle < Math.PI) {
+      angle += 2.0 * Math.PI;
+    }
+    return angle;
+  }
+
+  public ChezyMath() {
+  }
+}
---
+++ test/com/team254/lib/trajectory/test/TrajectoryGeneratorTests.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team254.lib.trajectory.test;
+
+import com.team254.lib.trajectory.Trajectory;
+import com.team254.lib.trajectory.TrajectoryGenerator;
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author jarussell
+ */
+public class TrajectoryGeneratorTests {
+
+  static void test(double start_vel, double goal_vel, double goal_distance,
+          TrajectoryGenerator.Strategy strategy) {
+    TrajectoryGenerator.Config config = new TrajectoryGenerator.Config();
+    config.dt = .01;
+    config.max_acc = 250.0;
+    config.max_jerk = 1250.0;
+    config.max_vel = 100.0;
+
+    Trajectory traj = TrajectoryGenerator.generate(
+            config,
+            strategy,
+            start_vel,
+            -75.0,
+            goal_distance,
+            goal_vel,
+            75.0);
+
+    System.out.print(traj.toString());
+
+    Trajectory.Segment last = traj.getSegment(traj.getNumSegments() - 1);
+    Assert.assertFalse(Math.abs(last.pos - goal_distance) > 1.0);
+    Assert.assertFalse(Math.abs(last.vel - goal_vel) > 1.0);
+    Assert.assertFalse(Math.abs(last.heading - 75.0) > 1.0);
+  }
+
+  public TrajectoryGeneratorTests() {
+  }
+
+  @BeforeClass
+  public static void setUpClass() {
+  }
+
+  @AfterClass
+  public static void tearDownClass() {
+  }
+
+  @Before
+  public void setUp() {
+  }
+
+  @After
+  public void tearDown() {
+  }
+
+  // Zero velocity endpoints
+  @Test
+  public void testP2PStep() {
+    test(100, 100, 120, TrajectoryGenerator.StepStrategy);
+  }
+  
+  @Test
+  public void testP2PShortStep() {
+    test(100, 100, 30, TrajectoryGenerator.StepStrategy);
+  }
+  
+  @Test
+  public void testP2PTrapezoid() {
+    test(0, 0, 120, TrajectoryGenerator.TrapezoidalStrategy);
+  }
+  
+  @Test
+  public void testP2PShortTrapezoid() {
+    test(0, 0, 30, TrajectoryGenerator.TrapezoidalStrategy);
+  }
+  
+  @Test
+  public void testP2PSCurves() {
+    test(0, 0, 120, TrajectoryGenerator.SCurvesStrategy);
+  }
+  
+  @Test
+  public void testP2PShortSCurves() {
+    test(0, 0, 30, TrajectoryGenerator.SCurvesStrategy);
+  }
+  
+  // Non-zero velocity endpoints
+  @Test
+  public void testRampUp() {
+    test(0, 100, 120, TrajectoryGenerator.TrapezoidalStrategy);
+  }
+  
+  @Test
+  public void testSlowRampUp() {
+    test(0, 50, 120, TrajectoryGenerator.TrapezoidalStrategy);
+  }
+  
+  @Test
+  public void testRampDown() {
+    test(100, 0, 120, TrajectoryGenerator.TrapezoidalStrategy);
+  }
+  
+  @Test
+  public void testSlowRampDown() {
+    test(50, 0, 120, TrajectoryGenerator.TrapezoidalStrategy);
+  }
+  
+  @Test
+  public void testRampUpDown() {
+    test(50, 50, 120, TrajectoryGenerator.TrapezoidalStrategy);
+  }
+  
+  @Test
+  public void testConstantVelTrapezoid() {
+    test(100, 100, 120, TrajectoryGenerator.TrapezoidalStrategy);
+  }
+}
