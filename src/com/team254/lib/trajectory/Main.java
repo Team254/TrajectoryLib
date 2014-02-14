23e465cdd6a1b81f00ea218fe1b3124524783454
Jared Russell <jared@team341.com>
2014-02-13 23:18:38 -0800

    Added main

---
+++ src/com/team254/lib/trajectory/Main.java
@@ -0,0 +1,29 @@
+package com.team254.lib.trajectory;
+
+import com.team254.lib.trajectory.io.JavaSerializer;
+
+/**
+ *
+ * @author Jared341
+ */
+public class Main {
+  public static void main() {
+    TrajectoryGenerator.Config config = new TrajectoryGenerator.Config();
+    config.dt = .01;
+    config.max_acc = 12.0;
+    config.max_jerk = 75.0;
+    config.max_vel = 5.0;
+    
+    Path p = new Path(10);
+    p.addWaypoint(new Path.Waypoint(0, 0, 0));
+    p.addWaypoint(new Path.Waypoint(10, 0, 0));
+    
+    Trajectory[] lr = PathGenerator.generateLeftAndRightFromPath(p, config,
+        25.5/12);
+
+    JavaSerializer js = new JavaSerializer();
+    String[] names = {"Left", "Right"};
+    String serialized = js.serialize("com.team254.frc2014.paths", names, lr);
+    System.out.print(serialized);
+  }
+}
