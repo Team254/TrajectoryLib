1447f7d774dc06c7e88cfaa35230b4296bd182a2
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2014-02-14 08:01:24 -0800

    3 ball path

--- src/com/team254/lib/trajectory/Main.java
+++ src/com/team254/lib/trajectory/Main.java
@@ -12,18 +12,19 @@ public class Main {
     config.dt = .01;
     config.max_acc = 12.0;
     config.max_jerk = 75.0;
-    config.max_vel = 5.0;
+    config.max_vel = 15.0;
     
     Path p = new Path(10);
     p.addWaypoint(new Path.Waypoint(0, 0, 0));
-    p.addWaypoint(new Path.Waypoint(10, 0, 0));
+    p.addWaypoint(new Path.Waypoint(6.5, 0, 0));
+    p.addWaypoint(new Path.Waypoint(15, 5, Math.PI / 6.0));
     
     Trajectory[] lr = PathGenerator.generateLeftAndRightFromPath(p, config,
         25.5/12);
 
     JavaSerializer js = new JavaSerializer();
     String[] names = {"Left", "Right"};
-    String serialized = js.serialize("com.team254.frc2014.paths", names, lr);
+    String serialized = js.serialize("com.team254.frc2014.paths.TestPath", names, lr);
     System.out.print(serialized);
   }
 }
