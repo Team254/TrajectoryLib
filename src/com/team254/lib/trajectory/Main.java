df1214b0900659d7e6b8e965f8d1dadb810c9549
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2014-04-09 21:04:31 -0700

    SVR shot

--- src/com/team254/lib/trajectory/Main.java
+++ src/com/team254/lib/trajectory/Main.java
@@ -66,7 +66,7 @@ public class Main {
       WaypointSequence p = new WaypointSequence(10);
       p.addWaypoint(new WaypointSequence.Waypoint(0, 0, 0));
       p.addWaypoint(new WaypointSequence.Waypoint(7.0, 0, 0));
-      p.addWaypoint(new WaypointSequence.Waypoint(13.5, 1.0, Math.PI / 12.0));
+      p.addWaypoint(new WaypointSequence.Waypoint(14.0, 1.0, Math.PI / 12.0));
 
       Path path = PathGenerator.makePath(p, config,
           kWheelbaseWidth, path_name);
@@ -97,7 +97,7 @@ public class Main {
       WaypointSequence p = new WaypointSequence(10);
       p.addWaypoint(new WaypointSequence.Waypoint(0, 0, 0));
       p.addWaypoint(new WaypointSequence.Waypoint(7.0, 0, 0));
-      p.addWaypoint(new WaypointSequence.Waypoint(13.5, 5.0, Math.PI / 12.0));
+      p.addWaypoint(new WaypointSequence.Waypoint(14.0, 5.0, Math.PI / 12.0));
 
       Path path = PathGenerator.makePath(p, config,
           kWheelbaseWidth, path_name);
